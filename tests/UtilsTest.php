2093994ddeaaab6ce4a2a091d16a6591fab943b3
Nikolay Kalashnikov <kalash-job@yandex.ru>
2022-01-06 00:03:31 +0300

    add Utils and UtilsTest

---
+++ tests/UtilsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Php\Package\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function Hexlet\Phpunit\Utils\reverseString;
+
+class UtilsTest extends TestCase
+{
+    // Метод, функция определенная внутри класса
+    // Должна начинаться со слова test
+    // public – чтобы PHPUnit мог вызвать этот тест снаружи
+    public function testReverse(): void
+    {
+        // Сначала идет ожидаемое значение (expected)
+        // И только потом актуальное (actual)
+        $this->assertEquals('', reverseString(''));
+        $this->assertEquals('olleh', reverseString('hello'));
+    }
+}
